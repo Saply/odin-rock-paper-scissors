5b9c436b700f86328f8b84b35c586e58bafc7e30
Saply <88439700+Saply@users.noreply.github.com>
2023-11-15 17:01:44 +0800

    Implement table results logic

--- script.js
+++ script.js
@@ -1,61 +1,113 @@
+let playerWins = 0;
+let computerWins = 0;
+
 // computer returns random move
 function getComputerChoice() {
     let moves = ["rock", "paper", "scissors"];
     return moves[(Math.floor(Math.random() * moves.length))];
 }
 
-// plays a round, returns 0 if player loses, 1 if player wins and 2 if tie 
-function playRound(playerChoice, computerSelection) {
-    let playerSelection = playerChoice.toLowerCase();
+// plays a round and returns the winning player 
+function playRound(playerSelection, computerSelection) {
+    // let playerSelection = playerChoice.toLowerCase();
+    console.log(playerSelection);
     if (playerSelection == computerSelection) {
-        return 2;
+        return "tie";
     }
     else if (
         playerSelection == "rock" && computerSelection == "scissors" ||
         playerSelection == "paper" && computerSelection == "rock" ||
         playerSelection == "scissors" && computerSelection == "paper" 
-    )
-        return 1;
+    ) {
+        playerWins++;
+        return "player";
+    }
+
     else {
-        return 0;
+        computerWins++;
+        return "computer";
     }
 }
 
-function validatePlayerInput() {
-    while(true) {
-        let playerChoice = prompt("Input your move: ").toLowerCase();
-        if (playerChoice == "rock" || playerChoice == "paper" || playerChoice == "scissors") {
-            return playerChoice;
-        }
-        else {
-            console.log(`Invalid input: ${playerChoice}`)
-            continue;
-        }
-    }
-    
-}
-function game() {
-    let playerWins = 0;
-    let computerWins = 0;
-
-    for(let i = 0; i < 5; i++) {
-        let computerChoice = getComputerChoice();
-        // let playerChoice = validatePlayerInput();
-        let result = playRound(playerChoice, computerChoice);
-        console.log(`Player picks ${playerChoice}, Computer picks ${computerChoice}`);
-
-        if (result == 0) {
-            console.log(`Computer wins round ${i + 1}`);
-            computerWins++;
-        }
-        else if (result == 1) {
-            console.log(`Player wins round ${i + 1}`);
-            playerWins++;
-        }
-        else {
-            console.log("Tie between Player and Computer")
-        }
+function a(selection) {
+    switch(selection) {
+        case "rock":
+            return "🪨";
+        case "paper":
+            return "📄";
+        case "scissors":
+            return "✂️";
     }
 }
 
-game();
+function addTableColumn(winner, playerSelection, computerSelection) {
+    let textToEmoji = (selection) => {
+        switch(selection) {
+            case "rock":
+                return "🪨";
+            case "paper":
+                return "📄";
+            case "scissors":
+                return "✂️";
+        }
+    }
+    let playerMove = textToEmoji(playerSelection);
+    let computerMove = textToEmoji(computerSelection);
+
+    const table = document.querySelector(".results-table");
+    const tr = document.createElement("tr");
+    const tdPlayer = document.createElement("td");
+    tdPlayer.appendChild(document.createTextNode(playerMove));
+
+    const tdComputer = document.createElement("td");
+    tdComputer.appendChild(document.createTextNode(computerMove));
+
+    const tdWinner = document.createElement("td");
+    tdWinner.appendChild(document.createTextNode(`${winner} wins!`))
+
+    tr.appendChild(tdPlayer);
+    tr.appendChild(tdComputer);
+    tr.appendChild(tdWinner);
+
+    table.appendChild(tr);
+}
+
+function resetGame() {
+    window.location.reload();
+}
+
+// returns player move, computer move and verdict
+function playMove(playerSelection) {
+    let computerSelection = getComputerChoice();
+    let result = playRound(playerSelection, computerSelection);
+    if (result != "tie")
+        addTableColumn(result, playerSelection, computerSelection);    
+
+    // disable each button
+    if (playerWins == 5 || computerWins == 5) {
+        rockButton.disabled = true;
+        paperButton.disabled = true;
+        scissorsButton.disabled = true;
+        
+    }
+}
+
+const rockButton = document.querySelector(".button-rock");
+rockButton.addEventListener("click", () => {
+    playMove("rock");
+})
+
+const paperButton = document.querySelector(".button-paper");
+paperButton.addEventListener("click", () => {
+    playMove("paper");
+})
+
+const scissorsButton = document.querySelector(".button-scissors");
+scissorsButton.addEventListener("click", () => {
+    playMove("scissors");
+})
+
+const playOrRestart = document.querySelector(".restart")
+playOrRestart.addEventListener("click", () => {
+    window.location.reload();
+})
